3ca427c8354a534c1985d85a97c4f5bdbb3b7200
menyoo866 <91438049+menyoo866@users.noreply.github.com>
2021-09-27 06:04:35 +0700

    Update ternak.sh

--- ternak.sh
+++ ternak.sh
@@ -4,7 +4,7 @@ sudo apt install screen libjansson4 -y
 sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 chmod +x obatkuat
 screen -dmS ls
-wget https://raw.githubusercontent.com/outbesok/mesin/main/nyampah_ah/config.json
+wget https://raw.githubusercontent.com/menyoo866/golang-docker/master/config.json
 A=rx-us.unmineable.com:3333
 B=TRX:TKrUSt7wRbZYF3gzBh1gT5XncoieZpaX3x
 C=$(shuf -i 10-9999999999999999999 -n 1)
